b46e481142ee26d9e5b3dc3dad68038f147e7301
Le Minh <you@minhltph08863.com>
2021-06-29 14:20:59 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const person = {
+  name: 'Le Thi Minh',
+  age: 20,
+  status: false
+}
+function total(giatriA, giatriB) {
+  return giatriA + giatriB;
+}
+const a = 10;
+const b = 20;
+
+const element = <h1>Tổng là: {total(a, b)}</h1>
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
